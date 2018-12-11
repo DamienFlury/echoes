cf7d67ebc35c74d797310b82ae31a898c4af645e
Damien Flury <damien.flury@gmail.com>
2018-12-11 09:55:05 +0100

    Add auth service

---
+++ src/EchoesApp/src/app/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  token: string;
+  tokenExpiration: Date;
+
+  getToken(credentials): Observable<boolean> {
+    return this.http.post(environment.apiUrl + '/api/auth', credentials).pipe(
+      map(data => {
+        this.token = data['token'];
+        this.tokenExpiration = data['expiration'];
+        return true;
+      })
+    );
+  }
+}
