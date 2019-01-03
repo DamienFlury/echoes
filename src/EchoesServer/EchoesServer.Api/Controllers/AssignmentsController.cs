bcb3a9b4489043339453772d821f7ef9c6c76930
Damien Flury <damien.flury@gmail.com>
2019-01-03 17:21:28 +0100

    Fix assignments

--- src/EchoesServer/EchoesServer.Api/Controllers/AssignmentsController.cs
+++ src/EchoesServer/EchoesServer.Api/Controllers/AssignmentsController.cs
@@ -10,38 +10,37 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EchoesServer.Api.Controllers
 {
-    [Route ("api/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class AssignmentsController : ControllerBase
     {
         private readonly SchoolContext _context;
 
-        public AssignmentsController (SchoolContext context) => _context = context;
+        public AssignmentsController(SchoolContext context) => _context = context;
 
         // GET api/values
         [HttpGet]
-        [Authorize (AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public ActionResult<IEnumerable<Assignment>> Get ()
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<IEnumerable<Assignment>> Get()
         {
-            var userName = User.Identity.Name;
-            return Ok (_context.Assignments.Where (assignment =>
-                assignment.StudentAssignments.Select (sa => sa.Student).Any (student => student.User.UserName == User.Identity.Name)));
+            return Ok(_context.Assignments.Where(assignment
+                => assignment.Class.StudentClasses.Select(sc => sc.Student).Any(student => student.User.UserName == User.Identity.Name)));
         }
 
         // GET api/values/5        
-        [HttpGet ("{id}")]
-        public ActionResult<Assignment> Get (int id) => _context.Assignments.Find (id);
+        [HttpGet("{id}")]
+        public ActionResult<Assignment> Get(int id) => _context.Assignments.Find(id);
 
-        [Authorize (AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public async Task<ActionResult> Post ([FromBody] Assignment assignment)
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> Post([FromBody] Assignment assignment)
         {
             // var classId = assignment.ClassId;
             // if (classId == 0) return BadRequest ();
             // var student = await _context.Students.SingleOrDefaultAsync(stud => stud.User.UserName == User.Identity.Name);
             // if(!student.StudentClasses.Any(sc => sc.StudentId == student.Id && sc.ClassId == classId)) return BadRequest();
-            await _context.Assignments.AddAsync (assignment);
-            await _context.SaveChangesAsync ();
-            return Ok (assignment);
+            await _context.Assignments.AddAsync(assignment);
+            await _context.SaveChangesAsync();
+            return Ok(assignment);
         }
     }
 }
